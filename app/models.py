dbfd2cc51ff0442062c903fa5a3f70a136b6d5c4
lorna nashipae <kimanilorna@yahoo.com>
2018-11-12 01:22:46 +0300

    This commit is to add classes to the model file and create tests for them

---
+++ app/models.py
@@ -0,0 +1,30 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,title,description,url):
+        self.id =id
+        self.title = title
+        self.description = description
+        self.url = url
+        
+
+
+
+class Article:
+
+    '''
+    Aticle class to define Article Objects
+    '''
+
+
+    def __init__(self,id,name,title,description,url,urlToImage):
+        self.id =id
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+
+
+    
