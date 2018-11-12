cdb1083586c232de8d9d233bda3fb6c392955e1e
lorna nashipae <kimanilorna@yahoo.com>
2018-11-12 09:05:47 +0300

    This commit is to create the landng page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,18 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_movies,get_movie,search_movie
-from .forms import ReviewForm
-from ..models import Review
+from ..requests import get_newsource
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    #Getting news sources
+    general_newsource = get_newsource('general')
+        technology_newsource = get_newsource('technology')
+    entertainment_newsource = get_newsource('entertainment')
+    sports_newsource = get_newsource('sports')
+
+    return render_template('index.html',general = general_newsource,general = general_newsource, technology = technology_newsource, entertainment = entertainment_newsource, sports = sports_newsource)
