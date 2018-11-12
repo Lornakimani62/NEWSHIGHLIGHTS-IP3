8784598ce2bacabcf527655223e270153d8505e9
lorna nashipae <kimanilorna@yahoo.com>
2018-11-12 07:43:43 +0300

    This commit is to create the get-newsource request and to also make changes to the base-url

---
+++ app/requests.py
@@ -0,0 +1,63 @@
+import urllib.request,json
+from datetime import datetime
+from .models import Source
+from .models import Article
+
+# Getting api key
+api_key = None
+# Getting the Headline url
+Headline_url = None
+#Getting the Everything url
+Everything_url = None
+#Search url
+search_url = None
+
+
+
+def configure_request(app):
+    global api_key,Everything_url,Headline_url
+    api_key = app.config['NEWS_API_KEY']
+    Headline_url = app.config['HEADLINE_BASE_URL']
+    Everything_url=app.config['EVERYTHING_BASE_URL']
+    search_url = app.config["SEARCH_API_BASE_URL"]
+
+def get_newsource(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_newsource_url = Everything_url.format(category,api_key)
+    print(get_newsource_url)
+
+    with urllib.request.urlopen(get_newsource_url) as url:
+        get_newsource_data = url.read()
+        get_newsource_response = json.loads(get_newsource_data)
+
+        newsource_results = None
+
+        if get_newsource_response['sources']:
+            newsource_results_list = get_newsource_response['sources']
+            newsource_results = process_results(newsource_results_list)
+
+    return newsource_results
+
+
+def process_results(newsource_list):
+    '''
+    Function  that processes the new source result and transform them to a list of Objects
+    Args:
+        newsource_list: A list of dictionaries that contain news source details
+    Returns :
+        newsource_results: A list of newsource objects
+    '''
+    newsource_results = []
+    for news_item in newsource_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+
+        newsource_object = Source(id,name,description,url)
+        newsource_results.append(newsource_object)
+
+    return newsource_results
+
--- config.py
+++ config.py
@@ -5,10 +5,11 @@ class Config:
     General configuration parent class
     '''
 
-    HEADLINE_BASE_URL='https://newsapi.org/v2/top-headlines?sources=bbc-news&language=en&apiKey=a3bd9e0f12ad45a79350ccc74aeee6eb'
-    EVERYTHING_BASE_URL='https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news&apiKey=a3bd9e0f12ad45a79350ccc74aeee6eb'
-    NEWS_API_KEY = os.environ.get('a3bd9e0f12ad45a79350ccc74aeee6eb')
-    SECRET_KEY = os.environ.get('0722212432')
+    HEADLINE_BASE_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    EVERYTHING_BASE_URL='https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news&sortBy=publishedAt&pageSize={}apiKey={}'
+    SEARCH_API_BASE_URL = 'https://newsapi.org/v2/everything?language=en&q={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     pass
